ac7c8e9e3a7e329286c7c0716b044cbf4bc0c5dc
cayniarb <charlton.danielk@gmail.com>
2011-09-15 16:16:49 -0400

    msm: acpuclock-8x60: paremeterize FREQ_TBL_SIZE

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -57,6 +57,7 @@
 #endif
 #define SCPLL_NOMINAL_VDD	1100000 /* uV */
 #define FREQ_MAX		1512000 /* Khz */
+#define FREQ_TBL_SIZE		30	/* number */
 
 /* SCPLL Modes. */
 #define SCPLL_POWER_DOWN	0
@@ -718,7 +719,7 @@ static void __init bus_init(void)
 }
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][30];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TBL_SIZE];
 
 static void __init cpufreq_table_init(void)
 {
