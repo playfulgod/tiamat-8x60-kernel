0cdf42d07cb18296d7476b4c4f8c984850cd32ba
cayniarb <charlton.danielk@gmail.com>
2012-03-06 12:27:57 -0500

    msm: board: set low_cap_limit to 5%

--- arch/arm/mach-msm/board-ruby.c
+++ arch/arm/mach-msm/board-ruby.c
@@ -1315,7 +1315,7 @@ static struct msm_camera_sensor_flash_src msm_flash_src = {
 
 static struct camera_flash_cfg msm_camera_sensor_flash_cfg = {
 	.low_temp_limit		= 5,
-	.low_cap_limit		= 15,
+	.low_cap_limit		= 5,
 };
 
 #ifdef CONFIG_S5K3H2YX
@@ -6008,4 +6008,3 @@ MACHINE_START(RUBY, "ruby")
 	.init_machine = ruby_init,
 	.timer = &msm_timer,
 MACHINE_END
-
